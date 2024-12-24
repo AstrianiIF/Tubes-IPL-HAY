ca1ac7adffad47b2fd9f716a0473781ad775619a
astrianikomeri33 <astrianikomeri33@gmail.com>
2024-12-24 20:30:35 +0700

    Memodifikasi main dan module-info

--- Koperasi/src/module-info.java
+++ Koperasi/src/module-info.java
@@ -1,5 +1,8 @@
 module Koperasi {
 	requires javafx.controls;
+	requires javafx.fxml;
+	exports UI to javafx.fxml;
 	
+	opens UI to javafx.fxml;
 	opens application to javafx.graphics, javafx.fxml;
 }
